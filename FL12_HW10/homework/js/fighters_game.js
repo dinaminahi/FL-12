e7bea7c39d897664ddac7fd1ecc4f6db32a62ae3
Diana Grechukh <46566190+dinaminahi@users.noreply.github.com>
2020-01-21 17:37:13 +0200

    add attack ang logCombatHistory methods

--- FL12_HW10/homework/js/fighters_game.js
+++ FL12_HW10/homework/js/fighters_game.js
@@ -2,7 +2,6 @@ function Fighter(obj) {
       obj.wins = 0;
       obj.loses = 0;
       obj.totalHP = obj.HP;
-
       return {
             getName: () => obj.name,
             getDamage: () => obj.damage,
@@ -12,11 +11,15 @@ function Fighter(obj) {
             heal: (points) => points + obj.HP > obj.totalHP ? obj.HP = obj.totalHP : obj.HP += points,
             dealDamage: (points) => obj.HP - points <= 0 ? obj.HP = 0 : obj.HP -= points,
             attack: (fighter) => {
-                 const probabilityOfSuccess = 1 - (fighter.getDamage() + fighter.getAgility())/100;
+                 const probabilityOfSuccess = 1 - (fighter.getDamage() + fighter.getAgility()) / 100;
                  if (Math.random() <= probabilityOfSuccess) {
-
+                    fighter.dealDamage(obj.damage;
+                    console.log(`${obj.name} makes ${obj.damage} damage to ${fighter.getName()}`);
+                 } else {
+                   console.log(`${fighter.getName()} attack missed`);
                  }
             },
+            logCombatHistory: () => console.log(`Name: ${obj.name}, Wins: ${obj.wins}, Loses: ${obj.loses}`),
             addWin: () => obj.wins++,
             addLose: () => obj.loses++
       }
