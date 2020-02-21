46b9a876cff7ca85f74cce5ce53b996fdebb6453
Diana Grechukh <46566190+dinaminahi@users.noreply.github.com>
2020-02-21 19:26:45 +0200

    Update index.js

--- FL12_Webinar_HW3/index.js
+++ FL12_Webinar_HW3/index.js
@@ -1,12 +1,28 @@
 const ranks = ['Ace', 2, 3, 4, 5, 6, 7, 8 ,9, 10, 'Jack', 'Queen', 'King'];
+const suits = ['hearts', 'diamonds', 'clubs', 'spades'];
+
+function createDeckOfCards() {
+    let deck = [];
+    for(let i = 0; i < suits.length; i++) {
+        for(let j = 0; j < ranks.length; j++) {
+           deck.push(new Card(suits[i],ranks[j]));
+        }
+    }
+    return deck;
+}
 
 class Card {
-    constructor(suit, rank, isFace) {
+    constructor(suit, rank) {
         this.suit = suit;
         this.rank = rank;
-        this.isFace = isFace;
     }
+
     get isFaceCard() {
+        if (this.rank === 1 || this.rank > 10){
+            this.isFace = true;
+        } else {
+            this.isFace = false;
+        }
         return this.isFace;
     }
 
@@ -17,4 +33,14 @@ class Card {
     static compare(cardOne, cardTwo){
         return `${cardOne.toString()} is ${cardOne.rank > cardTwo.rank ? 'bigger' : 'smaller'} than ${cardTwo.toString()}`;
     }
+}
+
+
+class Deck {
+  constructor() {
+      this.cards = createDeckOfCards();
+  } 
+  get count() {
+      return this.cards.length;
+  }
 }
