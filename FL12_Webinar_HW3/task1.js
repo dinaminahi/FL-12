911ec12bf24e698d6265c76188d539167432ee17
Diana Grechukh <46566190+dinaminahi@users.noreply.github.com>
2020-02-24 22:28:09 +0200

    Update task1.js

--- FL12_Webinar_HW3/task1.js
+++ FL12_Webinar_HW3/task1.js
@@ -1,16 +1,6 @@
 const ranks = ['Ace', 2, 3, 4, 5, 6, 7, 8 ,9, 10, 'Jack', 'Queen', 'King'];
 const suits = ['hearts', 'diamonds', 'clubs', 'spades'];
 
-function createDeckOfCards() {
-    let deck = [];
-    for(let i = 0; i < suits.length; i++) {
-        for(let j = 0; j < ranks.length; j++) {
-           deck.push(new Card(suits[i], j));
-        }
-    }
-    return deck;
-}
-
 class Card {
     constructor(suit, rank) {
         this.suit = suit;
@@ -31,17 +21,33 @@ class Card {
     }
 
     static compare(cardOne, cardTwo){
-        return cardOne.rank > cardTwo.rank;
+        if (cardOne.rank === 1){
+            return true;
+        } else if (cardTwo.rank === 1) {
+            return false;
+        } else {
+            return cardOne.rank > cardTwo.rank;
+        }
     }
 }
 
 class Deck {
   constructor() {
-      this.cards = createDeckOfCards();
+      this.cards = (function () {
+        let readyDeck = [];
+        for(let i = 0; i < suits.length; i++) {
+            for(let j = 0; j < ranks.length; j++) {
+                readyDeck.push(new Card(suits[i], j));
+            }
+        }
+        return readyDeck;
+      })();
   } 
+
   get count() {
       return this.cards.length;
   }
+
   suffle() {
       for( let i = 0; i < 100; i++) {
           let fRandNum =  Math.floor(Math.random()*52);
@@ -58,38 +64,49 @@ class Deck {
   }
 }
 
+const _wins = Symbol('wins');
+
 class Player {
-    constructor(name) {
+    constructor (name) {
         this.name = name;
         this.deck = new Deck();
-        this.wins = 0;
+        this[_wins] = 0;
     }
-    // get wins() {
-    //     return this.wins;
-    // }
+    
+    addWin() {
+        this[_wins]++;
+    }
+
+    get wins() {
+        return this[_wins];
+    }
+
     static Play(playerOne, playerTwo) {
         playerOne.deck.suffle();
         playerTwo.deck.suffle();
+
         let fPoints = 0;
         let sPoints = 0;
+
         for (let i = 0; i < 51; i++) {
             let fCard = playerOne.deck.draw(1);
             let sCard = playerTwo.deck.draw(1);
             if (Card.compare(fCard[0],sCard[0])) {
                 fPoints++;
-            } else if (!Card.compare(fCard[0],sCard[0])) {
+            } else if (Card.compare(sCard[0],fCard[0])) {
                 sPoints++;
             } else {
                 i--;
             }
         }
+        
         if (fPoints > sPoints) {
-            playerOne.wins++;
+            playerOne.addWin();
             playerOne.deck = new Deck();
             playerTwo.deck = new Deck();                       
             return `${playerOne.name} wins ${fPoints} to ${sPoints}`;
         } else if (sPoints > fPoints) {
-            playerTwo.wins++;
+            playerTwo.addWin();
             playerOne.deck = new Deck();
             playerTwo.deck = new Deck(); 
             return `${playerTwo.name} wins ${sPoints} to ${fPoints}`;
