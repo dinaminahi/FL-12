3be57be8cef968b682d088a179d31c996b70a39e
Diana Grechukh <46566190+dinaminahi@users.noreply.github.com>
2020-03-03 22:43:57 +0200

    add todolist actions

--- FL12_Webinar_HW6/task2/js/index.js
+++ FL12_Webinar_HW6/task2/js/index.js
@@ -12,3 +12,38 @@ const todos = [
     done: true
   }
 ];
+  
+(function ( $ ) {
+
+  $.fn.addItem = function() { 
+
+       this.on('click', function(e) {
+        e.preventDefault();
+        let text = $input.val();
+        $list.append(
+          `<li class="item">
+             <span class="item-text">${text}</span>
+             <button class="item-remove">Remove</button> 
+           </li>
+          `);
+          $input.val('');
+       });
+  };
+
+}( jQuery ));
+
+$(document).ready(function() {
+
+  $add.addItem();
+
+  $list.on('click', '.item-remove', function(e) {
+    e.preventDefault();
+    $(this).parent().remove();
+  });
+
+  $list.on('click', '.item-text', function() {
+    $(this).toggleClass('done');
+  });
+
+});
+
